2268ee4d3dbcd0aba35876b4b4d1c91e657b19d6
poundsmichaelscode <michealolayenikan@gmail.com>
2025-06-24 02:15:45 -0700

    quick fix

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,22 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import TaskManager from './pages/TaskManager'
+import ApiViewer from './pages/ApiViewer'
+import { ThemeProvider } from './context/ThemeContext'
 
-import { Routes, Route } from "react-router-dom";
-
-
-export default function App() {
+function App() {
   return (
-    <MainLayout>
+    <ThemeProvider>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tasks" element={<TaskManager />} />
-        <Route path="/posts" element={<PostsPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="tasks" element={<TaskManager />} />
+          <Route path="api" element={<ApiViewer />} />
+        </Route>
       </Routes>
-    </MainLayout>
-  );
+    </ThemeProvider>
+  )
 }
+
+export default App
